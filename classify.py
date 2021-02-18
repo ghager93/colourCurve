d43d9b018ef4aa710682cd5d68996704a5afd81f
ghage <ghager93@gmail.com>
2021-02-18 14:34:29 +1100

    image classification and sampling

--- classify.py
+++ classify.py
@@ -6,8 +6,8 @@ from sklearn.cluster import KMeans, MiniBatchKMeans
 from colour import *
 
 
-def classify(points, n_clusters=int):
-    return _by_mini_batch_k_means(points, n_clusters)
+def classify(points, n_clusters: int):
+    return _by_k_means(points, n_clusters)
 
 
 def _by_mini_batch_k_means(points, n_clusters: int):
@@ -18,3 +18,22 @@ def _by_mini_batch_k_means(points, n_clusters: int):
 def _by_k_means(points, n_clusters: int):
     k_means = KMeans(n_clusters=n_clusters).fit(points)
     return k_means.cluster_centers_, k_means.labels_
+
+
+def sample_image(image, n_samples):
+    sampling_ratio = n_samples / (image.shape[0] * image.shape[1])
+    sampling_mask = np.random.choice([False, True],
+                                    (image.shape[0], image.shape[1]),
+                                    p=[1 - sampling_ratio, sampling_ratio])
+    return image[sampling_mask]
+
+
+def image_centroids(image, n_centroids):
+    MAX_SAMPLE = 10000
+
+    if image.shape[0] * image.shape[1] > MAX_SAMPLE:
+        im_points = sample_image(image, MAX_SAMPLE)
+    else:
+        im_points = np.reshape(np.copy(image), (-1, 3))
+
+    return KMeans(n_centroids).fit(im_points).cluster_centres_
--- colour.py
+++ colour.py
@@ -11,6 +11,7 @@ def hsl2rgb(hue, saturation, lightness):
 def rgb2hsl(red, blue, green):
     return convert_rgb2hsl.rgb2hsl(red, blue, green)
 
+
 def hsl_square_grid(resolution=(256, 256), l=0.5, h_min=0, h_max=360, s_min=0, s_max=1, l_min=0, l_max=1):
     h = np.linspace(h_min, h_max, resolution[0], endpoint=False)
     s = np.linspace(s_min, s_max, resolution[1], endpoint=False)
@@ -27,6 +28,11 @@ def hsl_square_grid(resolution=(256, 256), l=0.5, h_min=0, h_max=360, s_min=0, s
 
     return h_1, s_1, l_1
 
+
 def rgb_square_grid(resolution=(256, 256), l=0.5, h_min=0, h_max=360, s_min=0, s_max=1, l_min=0, l_max=1):
     return hsl2rgb(*hsl_square_grid(resolution, l, h_min, h_max, s_min, s_max, l_min, l_max))
+
+
+def image2hsl(image):
+    return rgb2hsl(image.transpose(2, 0, 1)).transpose(1, 2, 0)
 
